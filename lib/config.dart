bb5edda26cf22cd42e49b753f8dd805e4621ef6f
pranav <43924596+pranstahub@users.noreply.github.com>
2023-05-21 19:53:49 +0530

    Delete lib directory

--- lib/config.dart
+++
@@ -1,7 +0,0 @@
-class Config {
-  String API_URL = "http://10.0.2.2:3100/Api";
-  String mapKey = "AIzaSyD6hhlx6VoL2n0HRnwuiTeZwuRN0_og0ys";
-}
-String API_URL = "http://10.0.2.2:3100/Api";
-String mapKey = "AIzaSyD6hhlx6VoL2n0HRnwuiTeZwuRN0_og0ys";
-  //String mapKey = "AIzaSyCUeKzBsQEKpCOxNAXe2z4aCKtR_aqBlqc";
