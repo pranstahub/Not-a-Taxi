931b9a259da6b5da0d0f61a541cdb9530f5dbee9
pranav <43924596+pranstahub@users.noreply.github.com>
2023-05-21 23:38:52 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:not_a_taxi/screens/driver/driver_menu.dart';
+import 'package:not_a_taxi/screens/role_selector.dart';
+import 'screens/login_page.dart';
+import 'screens/metamask.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(<DeviceOrientation>[
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Not A Taxi',
+      home: LoginPage(),
+      routes: {
+        'metaMask': (context) => metaMask(),
+        'role_selector': (context) => RoleSelect(),
+        'driver_home': (context) => driverHome(),
+      },
+    );
+  }
+
+}
