7222e2ee9beb61b949b8826fe2090d26ee5dc27f
pranav <43924596+pranstahub@users.noreply.github.com>
2023-05-22 11:17:07 +0530

    Delete lib directory

--- lib/utils/constants.dart
+++
@@ -1,9 +0,0 @@
-// ignore_for_file: non_constant_identifier_names
-
-String infura_url = "https://sepolia.infura.io/v3/79698375b8de41c9ade316793ba29b18";
-
-String owner_private_key = "91cbb1ddfecadc1b49bbac4aa66fa591ee4e1333685f565456561b9429ebb2de";
-
-String contractAddress1 = "0x58E0Bd539912462FC3F188010b6FB9b106b8da1a";
-
-
