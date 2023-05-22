83befadb9756991111697734e4a07fcf7c1fb8c3
pranav <43924596+pranstahub@users.noreply.github.com>
2023-05-22 23:15:17 +0530

    Delete lib directory

--- lib/utils/globals.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:ffi';
-
-String userID= "";
-
-double passengerDepartureLatitude = 0.0;
-double passengerDepartureLongitude = 0.0;
-double passengerDestinationLatitude = 0.0;
-double passengerDestinationLongitude = 0.0;
