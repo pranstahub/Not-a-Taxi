4cec1718ff78259b91826dab02fffd6018bc802d
pranav <43924596+pranstahub@users.noreply.github.com>
2023-05-22 23:16:10 +0530

    Add files via upload

---
+++ lib/utils/globals.dart
@@ -0,0 +1,11 @@
+
+String userID= "";
+String globalDestination = "";
+
+double passengerDepartureLatitude = 0.0;
+double passengerDepartureLongitude = 0.0;
+double passengerDestinationLatitude = 0.0;
+double passengerDestinationLongitude = 0.0;
+
+
+int globalFare = 100;
