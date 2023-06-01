5b7d54c898cae8963b7dc80e771e2ab45214b144
pranav <43924596+pranstahub@users.noreply.github.com>
2023-06-01 19:44:24 +0530

    Update config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -1,7 +1,6 @@
 class Config {
-  String API_URL = "http://10.0.2.2:3100/Api";
-  String mapKey = "AIzaSyD6hhlx6VoL2n0HRnwuiTeZwuRN0_og0ys";
+  String API_URL = "YOUR-URL";
+  String mapKey = "YOUR-API-KEY";
 }
-String API_URL = "http://10.0.2.2:3100/Api";
-String mapKey = "AIzaSyD6hhlx6VoL2n0HRnwuiTeZwuRN0_og0ys";
-  //String mapKey = "AIzaSyCUeKzBsQEKpCOxNAXe2z4aCKtR_aqBlqc";
+String API_URL = "YOUR-URL";
+String mapKey = "YOUR-API-KEY";
